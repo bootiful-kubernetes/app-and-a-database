f73b3af4d93474defa47cff11a14ef05fb49a06d
Josh Long <josh@joshlong.com>
2020-10-15 00:34:22 -0700

    retries

--- local/run_mysql.sh
+++ local/run_mysql.sh
@@ -2,6 +2,15 @@
 
 CN=mysql-db
 PW=password
-docker run --restart always --name mysqldb -v `pwd`/data:/var/lib/mysql -p 3306:3306 -d -e MYSQL_ROOT_PASSWORD=$PW mysql:8.0.20
+
+rm -rf `pwd`/data
+
+docker run --restart always -d  --name mysqldb \
+ -e MYSQL_USER=bp \
+ -e MYSQL_PASSWORD=bp \
+ -e MYSQL_ROOT_PASSWORD=root \
+ -e MYSQL_DATABASE=bp    \
+ -p 3306:3306  \
+ -v `pwd`/data:/var/lib/mysql mysql:8.0.20
 echo "the password is '${PW}'."
 
--- src/main/kotlin/com/example/app/AppApplication.kt
+++ src/main/kotlin/com/example/app/AppApplication.kt
@@ -1,6 +1,8 @@
 package com.example.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.availability.AvailabilityChangeEvent
+import org.springframework.boot.availability.ReadinessState
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
 import org.springframework.context.ApplicationListener
@@ -15,7 +17,9 @@ import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.body
 import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.Flux
+import reactor.kotlin.extra.retry.retryExponentialBackoff
 import java.io.InputStreamReader
+import java.time.Duration
 
 @SpringBootApplication
 class AppApplication
@@ -25,13 +29,14 @@ fun main(args: Array<String>) {
 		addInitializers(beans {
 
 			bean {
+				ApplicationListener<AvailabilityChangeEvent<ReadinessState>> {
+					println("new readinessState change ${it.state} ${it.resolvableType} ${it.source}")
+				}
 				ApplicationListener<ApplicationReadyEvent> {
 					val env = ref<Environment>()
 					val dbc = ref<DatabaseClient>()
 					val cr = ref<CustomerRepository>()
-
-					println(env ["spring.r2dbc.url"])
-
+					println(env["spring.r2dbc.url"])
 					ClassPathResource("/schema.sql").inputStream.use {
 						InputStreamReader(it).use { ir ->
 							val sql = ir.readText()
@@ -39,10 +44,10 @@ fun main(args: Array<String>) {
 									.execute(sql).fetch().rowsUpdated()
 									.thenMany(cr.saveAll(Flux.just(Customer(null, "A"), Customer(null, "B"))))
 									.thenMany(cr.findAll())
+									.retryExponentialBackoff(10, Duration.ofSeconds(10), Duration.ofMinutes(1), true)
 									.subscribe { println(it) }
 						}
 					}
-
 				}
 			}
 			bean {
